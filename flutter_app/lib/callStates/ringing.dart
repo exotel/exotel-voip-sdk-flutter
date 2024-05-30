60a749d6f1ff3192506b76b8a907c1e5ffb445f9
afrahmahmud <afrah.mahmud@exotel.com>
2024-05-30 17:21:34 +0530

    my_background_plugin

--- flutter_app/lib/callStates/ringing.dart
+++ flutter_app/lib/callStates/ringing.dart
@@ -42,47 +42,49 @@ class _RingingState extends State<Ringing> {
         backgroundColor: const Color(0xFF0800AF), // Set the app bar color to deep blue
       ),
       body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center, // This will center the column horizontally
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.2, vertical: MediaQuery.of(context).size.height * 0.1),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.1, bottom: MediaQuery.of(context).size.height * 0.02),
-                    child: Text(dialTo!, style: TextStyle(fontSize: MediaQuery.of(context).size.width * 0.08)),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.1, bottom: MediaQuery.of(context).size.height * 0.02),
-                    child: Text('$state', style: TextStyle(fontSize: MediaQuery.of(context).size.width * 0.06)),
-                  ),
-                  SizedBox(height: MediaQuery.of(context).size.height * 0.1),
-                  Padding(
-                    padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.05, bottom: MediaQuery.of(context).size.height * 0.02),
-                    child: GestureDetector(
-                      onTap:() {
-                        ApplicationUtils.getInstance(context).hangup();
-                        Navigator.pushReplacementNamed(
-                          context,
-                          '/home',
-                        );
-                      },
-                      child: ClipOval(
-                        child: Image.asset(
-                          'assets/btn_hungup_normal.png',
-                          width: MediaQuery.of(context).size.width * 0.15,
-                          height: MediaQuery.of(context).size.width * 0.15,
-                          fit: BoxFit.cover,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center, // This will center the column horizontally
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.2, vertical: MediaQuery.of(context).size.height * 0.1),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.1, bottom: MediaQuery.of(context).size.height * 0.02),
+                      child: Text(dialTo!, style: TextStyle(fontSize: MediaQuery.of(context).size.width * 0.08)),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.1, bottom: MediaQuery.of(context).size.height * 0.02),
+                      child: Text('$state', style: TextStyle(fontSize: MediaQuery.of(context).size.width * 0.06)),
+                    ),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+                    Padding(
+                      padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.05, bottom: MediaQuery.of(context).size.height * 0.02),
+                      child: GestureDetector(
+                        onTap:() {
+                          ApplicationUtils.getInstance(context).hangup();
+                          Navigator.pushReplacementNamed(
+                            context,
+                            '/home',
+                          );
+                        },
+                        child: ClipOval(
+                          child: Image.asset(
+                            'assets/btn_hungup_normal.png',
+                            width: MediaQuery.of(context).size.width * 0.15,
+                            height: MediaQuery.of(context).size.width * 0.15,
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
