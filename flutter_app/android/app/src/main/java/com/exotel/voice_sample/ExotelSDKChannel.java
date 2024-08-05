2d13d37f1d28d535d3a6974676ff9c5fca3665db
Afrahexotel <afrah.mahmud@exotel.com>
2024-08-05 11:11:07 +0530

    onDestroyMediaSession()

--- flutter_app/android/app/src/main/java/com/exotel/voice_sample/ExotelSDKChannel.java
+++ flutter_app/android/app/src/main/java/com/exotel/voice_sample/ExotelSDKChannel.java
@@ -285,6 +285,13 @@ public class ExotelSDKChannel implements VoiceAppStatusEvents,CallEvents, LogUpl
     }
 
     @Override
+    public void onDestroyMediaSession() {
+        uiThreadHandler.post(()->{
+            channel.invokeMethod(MethodChannelInvokeMethod.ON_DESTROY_MEDIA_SESSION,null);
+        });
+    }
+
+    @Override
     public void onDeInitialization() {
         uiThreadHandler.post(()->{
             channel.invokeMethod(MethodChannelInvokeMethod.ON_DEINITIALIZED,null);
